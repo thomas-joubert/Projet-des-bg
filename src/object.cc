434e6ac1d0dca6f3ce69227b2bd14422fe9cb293
thomas.joubert <thomas.joubert@epita.fr>
2020-09-26 12:29:49 +0200

    [Object] : wrote first canvas of class Object

---
+++ src/object.cc
@@ -0,0 +1,5 @@
+#include "object.hh"
+
+Object::Object(int x, int y)
+    : object_x_(x), object_y_(y)
+{}
---
+++ src/object.hh
@@ -0,0 +1,11 @@
+#pragma once
+
+class Object
+{
+    public :
+        Object(int x, int y);
+
+    protected :
+        int object_x_;
+        int object_y_;
+}
